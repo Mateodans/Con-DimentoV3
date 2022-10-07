568e84f22afdea001a91be5ce3c70f06728e8683
Mateodans <mateodand@gmail.com>
2022-10-07 02:37:43 -0300

    comienzo de crud

--- routes/admin.php
+++ routes/admin.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\CategoryController;
 
-route::get('', [HomeController::class, 'index']);
+route::get('', [HomeController::class, 'index'])->name('admin.home');
+
+route::resource('categories', CategoryController::class)->names('admin.categories');
